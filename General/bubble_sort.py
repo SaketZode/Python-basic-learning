dd0aa706b93ae801effb404f8ce4b9a140b66fe0
Saket Zode <55745771+SaketZode@users.noreply.github.com>
2020-08-16 02:04:07 +0530

    bubble sort corrected

--- General/bubble_sort.py
+++ General/bubble_sort.py
@@ -1,5 +1,5 @@
 def bubble_sort(lst):
-    for i in range(len(lst)-1):
+    for i in range(len(lst)-1, 0, -1):
         for j in range(len(lst)-1):
             if lst[j] > lst[j+1]:
                 lst[j], lst[j+1] = lst[j+1], lst[j]
