dd9d10dc9aefa47d61a2ecbaecd701bdcb15ae34
Saket Zode <55745771+SaketZode@users.noreply.github.com>
2020-08-16 01:48:32 +0530

    bubble sort

---
+++ General/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(lst):
+    for i in range(len(lst)-1):
+        for j in range(len(lst)-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    return lst
+
+
+elements = [45, 95, 12, 32, 21, 78, 56, 54, 102, 259, 99, 33]
+bubble_sort(elements)
+print(elements)
